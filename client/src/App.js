1406b59caade859a2fb11b3544221699d3fd4e0a
alexcsych <alexsejsi4@gmail.com>
2024-01-20 16:30:57 +0200

    Add Home LogIn SignUp routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import LogIn from './pages/LogIn'
+import SignUp from './pages/SignUp'
+
 function App () {
-  return <div></div>
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<LogIn />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
