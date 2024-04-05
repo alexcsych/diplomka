3ed51e60d16d2165e59379eb980eab92299e65dc
alexcsych <alexsejsi4@gmail.com>
2024-04-05 13:52:50 +0300

    Add /img

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const app = express()
 app.use(cors())
 
 app.use(express.json())
+app.use('/img', express.static('./public/images'))
 app.use('/api', router)
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(errorHandlers.errorHandler)
