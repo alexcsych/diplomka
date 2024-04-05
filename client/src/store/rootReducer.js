413f553203d9e868b1f6f03771f2fd67b709b69f
alexcsych <alexsejsi4@gmail.com>
2024-04-05 13:51:37 +0300

    Add Category store

--- client/src/store/rootReducer.js
+++ client/src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import userReducer from './slices/userSlice'
+import categoryReducer from './slices/categorySlice'
 
 const rootReducer = combineReducers({
-  userData: userReducer
+  userData: userReducer,
+  categoryData: categoryReducer
 })
 
 export default rootReducer
---
+++ client/src/store/slices/categorySlice.js
@@ -0,0 +1,55 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+export const getCategory = createAsyncThunk(
+  'category/getCategory',
+  async (err, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI
+    try {
+      const response = await axios.get(
+        'http://localhost:5000/api/category/getCategory'
+      )
+      return response.data
+    } catch (err) {
+      const errorData = err?.response?.data ?? 'Gateway Timeout'
+      const errorStatus = err?.response?.status ?? 504
+      return rejectWithValue({ data: errorData, status: errorStatus })
+    }
+  }
+)
+
+const categorySlice = createSlice({
+  name: 'category',
+  initialState: {
+    categoryData: [],
+    isFetching: false,
+    error: null
+  },
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(getCategory.pending, state => {
+        state.isFetching = true
+        state.error = null
+      })
+      .addCase(getCategory.fulfilled, (state, action) => {
+        const { categories } = action.payload.data
+        console.log('categories :>> ', categories)
+        state.isFetching = false
+        state.categoryData = categories
+      })
+      .addCase(getCategory.rejected, (state, action) => {
+        state.isFetching = false
+        state.error = action.payload.data.errors
+      })
+  }
+})
+
+const {
+  reducer
+  //  actions
+} = categorySlice
+
+// export const {} = actions
+
+export default reducer
