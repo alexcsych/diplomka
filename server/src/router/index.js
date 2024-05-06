d506f5e8ebc7f51357a2218c9d80053fdf9ee023
alexcsych <alexsejsi4@gmail.com>
2024-05-06 12:31:38 +0300

    Improve router

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 const usersRouter = require('./usersRouter')
 const categoriesRouter = require('./categoriesRouter')
+const itemsRouter = require('./itemsRouter')
 
 const router = Router()
 
 router.use('/user', usersRouter)
 router.use('/category', categoriesRouter)
+router.use('/item', itemsRouter)
 
 module.exports = router
