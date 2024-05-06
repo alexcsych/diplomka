8b5d88be1e717ba7fcbfaa4046e8fb045b6c4252
alexcsych <alexsejsi4@gmail.com>
2024-05-06 12:27:19 +0300

    Improve Home

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,50 +1,23 @@
-import React, { useEffect } from 'react'
-import styles from './Home.module.sass'
-import { getUser } from '../../store/slices/userSlice'
+import React from 'react'
 import Header from '../../components/Header'
-import { connect } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
 import Banner from '../../components/Banner'
 import SearchBar from '../../components/SearchBar'
 import Category from '../../components/Category'
-import { getCategory } from '../../store/slices/categorySlice'
+import styles from './Home.module.sass'
 
-function Home ({ getUser, getCategory, error }) {
-  const navigate = useNavigate()
-  useEffect(() => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      getUser(token)
-    }
-    getCategory()
-  }, [getUser, getCategory])
-
-  useEffect(() => {
-    if (error) {
-      localStorage.removeItem('token')
-      navigate('/login')
-    }
-  }, [error, navigate])
-
+function Home () {
   return (
     <>
       <Header></Header>
       <div className={styles.page}>
         <Banner></Banner>
-        <SearchBar></SearchBar>
-        <Category></Category>
+        <div className={styles.container}>
+          <SearchBar></SearchBar>
+          <Category></Category>
+        </div>
       </div>
     </>
   )
 }
 
-const mapStateToProps = state => {
-  return { error: state.userData.error }
-}
-
-const mapDispatchToProps = dispatch => ({
-  getUser: token => dispatch(getUser(token)),
-  getCategory: () => dispatch(getCategory())
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default Home
