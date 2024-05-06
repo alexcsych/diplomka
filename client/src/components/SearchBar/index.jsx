c84ef78a4ece3258fbde657252fbbb66ae91024f
alexcsych <alexsejsi4@gmail.com>
2024-05-06 12:26:20 +0300

    Add SearchBar

--- client/src/components/SearchBar/index.jsx
+++ client/src/components/SearchBar/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import styles from './SearchBar.module.sass'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 function SearchBar () {
   return (
-    <div className={styles.box}>
-      <div className={styles.container}>
-        <input className={styles.input} type='text' />
-        <button className={styles.btn}>Search</button>
-      </div>
+    <div className={styles.container}>
+      <input className={styles.input} type='text' />
+      <button className={styles.btn}>
+        <FontAwesomeIcon icon={faSearch} className={styles.icon} />
+      </button>
     </div>
   )
 }
