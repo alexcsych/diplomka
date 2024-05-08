8e3d55b435cf670e85d437eaea50279cd49a24c5
alexcsych <alexsejsi4@gmail.com>
2024-05-08 14:55:19 +0300

    Update itemsController

--- server/src/controllers/itemsController.js
+++ server/src/controllers/itemsController.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
-// const createHttpError = require('http-errors')
-const { Item } = require('../models')
+const createHttpError = require('http-errors')
+const { Item, Rating } = require('../models')
+const { mongoose } = require('mongoose')
 const ITEMS_PER_PAGE = 6
 
 module.exports.getItemsByCategory = async (req, res, next) => {
   const { categoryId } = req.params
   const { itemName, fPrice, lPrice, otherFilter, page } = req.query.filter
+  const pageNumber = parseInt(page, 10) || 1
+
   console.log('req.query :>> ', req.query)
   console.log('req.params :>> ', req.params)
   console.log('otherFilter :>> ', otherFilter)
@@ -18,7 +21,7 @@ module.exports.getItemsByCategory = async (req, res, next) => {
       ...(fPrice && lPrice
         ? { price: { $gte: parseFloat(fPrice), $lte: parseFloat(lPrice) } }
         : {}),
-      type: categoryId
+      type: new mongoose.Types.ObjectId(categoryId)
     }
 
     if (otherFilter) {
@@ -34,9 +37,26 @@ module.exports.getItemsByCategory = async (req, res, next) => {
     const totalItemsCount = await Item.countDocuments(filter)
     const maxPage = Math.ceil(totalItemsCount / ITEMS_PER_PAGE)
 
-    const foundItems = await Item.find(filter, '-createdAt -updatedAt -__v')
-      .limit(ITEMS_PER_PAGE)
-      .skip(ITEMS_PER_PAGE * (page - 1))
+    const foundItems = await Item.aggregate([
+      { $match: filter },
+      {
+        $lookup: {
+          from: 'ratings',
+          localField: '_id',
+          foreignField: 'item',
+          as: 'ratings'
+        }
+      },
+      {
+        $addFields: {
+          rating: { $avg: '$ratings.rating' }
+        }
+      },
+      { $unset: 'ratings' },
+      { $skip: ITEMS_PER_PAGE * (pageNumber - 1) },
+      { $limit: ITEMS_PER_PAGE }
+    ])
+    console.log('foundItems :>> ', foundItems)
 
     res.status(200).send({ data: { items: foundItems, maxPage } })
   } catch (err) {
@@ -64,6 +84,33 @@ module.exports.getFilterByCategory = async (req, res, next) => {
     }, {})
 
     res.status(200).send({ data: { filter: uniqueParams } })
+  } catch (err) {
+    next(err)
+  }
+}
+
+module.exports.getItemById = async (req, res, next) => {
+  const { _id } = req.params
+
+  try {
+    const item = await Item.findOne({ _id })
+
+    if (!item) {
+      return next(createHttpError(404, 'Item not found'))
+    }
+
+    const ratings = await Rating.find({ item: _id })
+    const rating = ratings.length
+      ? ratings.reduce((acc, cur) => acc + cur.rating, 0) / ratings.length
+      : null
+
+    const itemWithRating = {
+      ...item.toObject(),
+      rating,
+      views: ratings.length ? ratings.length : 0
+    }
+
+    res.status(200).send({ data: { item: itemWithRating } })
   } catch (err) {
     next(err)
   }
