652248649761e0b4513776b6420ffd3948594c52
alexcsych <alexsejsi4@gmail.com>
2024-05-09 17:36:17 +0300

    Add updateUser to usersRouter

--- server/src/router/usersRouter.js
+++ server/src/router/usersRouter.js
@@ -3,6 +3,19 @@ const usersRouter = Router()
 const { usersController } = require('./../controllers')
 const { validators } = require('../middleware')
 const { checkToken } = require('../middleware/checkToken')
+const path = require('path')
+const multer = require('multer')
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images')
+  },
+  filename: function (req, file, cb) {
+    const extension = path.extname(file.originalname)
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+    cb(null, 'userPhoto-' + uniqueSuffix + extension)
+  }
+})
+const upload = multer({ storage: storage })
 
 usersRouter
   .route('/signup')
@@ -14,8 +27,13 @@ usersRouter
 
 usersRouter.route('/getUser').get(checkToken, usersController.getUser)
 
-// usersRouter
-//   .route('/:_id')
-//   .patch(validators.validateUpdateData, usersController.updateUser)
+usersRouter
+  .route('/updateUser')
+  .patch(
+    checkToken,
+    upload.single('userImage'),
+    validators.validateUpdateData,
+    usersController.updateUser
+  )
 
 module.exports = usersRouter
