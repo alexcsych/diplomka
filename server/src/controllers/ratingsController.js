68b64bdc7755d6fe8a23a2241610ea00fd676588
alexcsych <alexsejsi4@gmail.com>
2024-05-09 17:34:51 +0300

    Update getComments

--- server/src/controllers/ratingsController.js
+++ server/src/controllers/ratingsController.js
@@ -4,10 +4,10 @@ const { Rating } = require('../models')
 module.exports.getComments = async (req, res, next) => {
   const { item } = req.params
   try {
-    const comments = await Rating.find({ item: item }).populate(
-      'user',
-      'userName'
-    )
+    const comments = await Rating.find({ item: item }).populate({
+      path: 'user',
+      select: ['userName', 'userImage']
+    })
     console.log('comments :>> ', comments)
     res.status(200).send({ data: { comments: comments } })
   } catch (err) {
